0c397d811058e567fd551a52fd60db0332f05693
CauaSilva28 <caua.santos56@etec.sp.gov.br>
2024-04-21 23:18:47 -0300

    Adicionando tema ligh e dark

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
+const toggleTheme = document.querySelector('#toggletheme');
+const toggleThemeMobile = document.querySelector('#toggletheme-mobile');
+const rootHtml = document.documentElement;
+
+const headerLogo = document.getElementById('header-logo');
+
 const navbarMobile = document.getElementById('nav-mobile'); 
 const iconNav = document.getElementById('navbar-mobile');
 const iconNav2 = document.getElementById('navbar-mobile2');
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 900) {
+        navbarMobile.style.display = 'none';
+    }
+});
+
+//Alternar modo light e dark
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute('data-theme');
+
+    if (currentTheme === 'light') {
+        rootHtml.setAttribute('data-theme', 'dark');
+        headerLogo.src ='img/logodark.png';
+    } 
+    else {
+        rootHtml.setAttribute('data-theme', 'light');
+        headerLogo.src = 'img/logolight.png';
+    }
+
+    toggleTheme.classList.toggle('bi-moon-fill');
+    toggleTheme.classList.toggle('bi-sun');
+
+    toggleThemeMobile.classList.toggle('bi-moon-fill');
+    toggleThemeMobile.classList.toggle('bi-sun');
+}
+
+toggleTheme.addEventListener('click', changeTheme);
+toggleThemeMobile.addEventListener('click', changeTheme);
 
 function AtivarNav(){
     navbarMobile.style.display = 'flex';
@@ -14,9 +49,3 @@ function DesativarNav(){
 
 iconNav.addEventListener('click', AtivarNav);
 iconNav2.addEventListener('click', DesativarNav);
-
-window.addEventListener('resize', function() {
-    if (window.innerWidth > 900) {
-        navbarMobile.style.display = 'none';
-    }
-});
