e66260708454daed3c021fe1fce2ffea5c4ae800
CauaSilva28 <caua.santos56@etec.sp.gov.br>
2024-05-02 21:00:52 -0300

    Update responsividade

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ const navbarMobile = document.getElementById('nav-mobile');
 const iconNav = document.getElementById('navbar-mobile');
 const iconNav2 = document.getElementById('navbar-mobile2');
 
+const btnstory1 = document.getElementById('story-btn1');
+const btnstory2 = document.getElementById('story-btn2');
+const btnstory3 = document.getElementById('story-btn3');
+const storyimg = document.getElementById('story-imagenav');
+const storyimg2 = document.getElementById('story-imagenav2');
+const storyimg3 = document.getElementById('story-imagenav3');
+
 //Alternar modo light e dark
 function changeTheme(){
     const currentTheme = rootHtml.getAttribute('data-theme');
@@ -45,8 +52,60 @@ function DesativarNav(){
 iconNav.addEventListener('click', AtivarNav);
 iconNav2.addEventListener('click', DesativarNav);
 
+//Botões passar imagem story
+btnstory1.style.background = 'var(--default-color)';
+btnstory1.style.scale = '1.3';
+
+function passarbtn1(){
+    storyimg.style.display = 'flex';
+    storyimg2.style.display = 'none';
+    storyimg3.style.display = 'none';
+    btnstory1.style.backgroundColor = 'var(--default-color)';
+    btnstory1.style.scale = '1.3';
+    btnstory2.style.backgroundColor = '#fff';
+    btnstory2.style.scale = '1';
+    btnstory3.style.backgroundColor = '#fff';
+    btnstory3.style.scale = '1';
+}
+function passarbtn2(){
+    storyimg2.style.display = 'flex';
+    storyimg.style.display = 'none';
+    storyimg3.style.display = 'none';
+    btnstory2.style.backgroundColor = 'var(--default-color)';
+    btnstory2.style.scale = '1.3';
+    btnstory1.style.backgroundColor = '#fff';
+    btnstory1.style.scale = '1';
+    btnstory3.style.backgroundColor = '#fff';
+    btnstory3.style.scale = '1';
+}
+function passarbtn3(){
+    storyimg3.style.display = 'flex';
+    storyimg2.style.display = 'none';
+    storyimg.style.display = 'none';
+    btnstory3.style.backgroundColor = 'var(--default-color)';
+    btnstory3.style.scale = '1.3';
+    btnstory2.style.backgroundColor = '#fff';
+    btnstory2.style.scale = '1';
+    btnstory1.style.backgroundColor = '#fff';
+    btnstory1.style.scale = '1';
+}
+
+btnstory1.addEventListener('click', passarbtn1);
+btnstory2.addEventListener('click', passarbtn2);
+btnstory3.addEventListener('click', passarbtn3);
+
+//resize
 window.addEventListener('resize', function() {
     if (window.innerWidth > 900) {
         navbarMobile.style.display = 'none';
+        storyimg.style.display = 'flex';
+        storyimg2.style.display = 'flex';
+        storyimg3.style.display = 'flex';
+    }
+    else{
+        passarbtn1();
+        storyimg.style.display = 'flex';
+        storyimg2.style.display = 'none';
+        storyimg3.style.display = 'none';
     }
 });
